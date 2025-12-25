b501f3bc0ba342c5fb9e0fa96c50e287bdb7745a
Sourav Jana <164884963+sourav2727@users.noreply.github.com>
2025-12-25 19:00:46 +0530

    Time: 331 ms (55.83%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,9 +1,17 @@
 # Write your MySQL query statement below
-with cte as (select managerId,count(*) as total_count
-from Employee
-where managerId is not null
-group by managerId)
 
 select e.name
-from cte c join Employee e on c.managerId=e.id
-where c.total_count>=5
+from Employee e
+join Employee m on e.id=m.managerId
+group by e.id,e.name
+having count(*)>=5
+
+
+-- with cte as (select managerId,count(*) as total_count
+-- from Employee
+-- where managerId is not null
+-- group by managerId)
+
+-- select e.name
+-- from cte c join Employee e on c.managerId=e.id
+-- where c.total_count>=5

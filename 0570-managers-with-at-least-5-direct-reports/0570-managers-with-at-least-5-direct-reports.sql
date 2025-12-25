d6c6467444dbe4f8bdf8f0f51f55c1b99206b0f4
Sourav Jana <164884963+sourav2727@users.noreply.github.com>
2025-12-25 18:47:41 +0530

    Time: 317 ms (77.96%), Space: 0B (100%) - LeetHub

---
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+with cte as (select managerId,count(*) as total_count
+from Employee
+group by managerId)
+
+select e.name
+from cte c join Employee e on c.managerId=e.id
+where c.total_count>=5
